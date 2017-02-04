97cee6312f26a6d2e045319dbb80f08eb3c8ec05
fi3 <fmerli1@gmail.com>
2017-02-04 14:22:11 +0100

    Implemented write in NodeHook

--- js/NodeHook.js
+++ js/NodeHook.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 
 class NodeHook {
+  //
   // Node hook for read and write the file where is stroed the db when the enviornment is Node
+  //
   constructor(path) {
     this._path = path;
     this._fileDescriptor = fs.openSync(path, 'r+');
@@ -12,33 +14,29 @@ class NodeHook {
   }
 
   close() {
-    //close file
+    fs.closeSync(this._fileDescriptor);
   }
-//fs.writeSync(fd, buffer2, 0, 2, 5);
 }
 
 function _slice(NodeHook, start, end) {
-  // return a buffer that start at file's byte start at end at file's byte end
+  //
+  // Return a buffer that start at file's byte position start at end at file's byte position end
+  //
   const length = end - start + 1;
   const data = Buffer.allocUnsafe(length);
   fs.readSync(NodeHook._fileDescriptor, data, 0, length, start);
   return data;
 }
 
-//  return StoredDb._hook.write(value, writePosition);
-//  return StoredDb._hook.writePosition(newNextPosition, writePosition);
-
-//function _write(buffer, offset) {
-//  //
-//  // Write buffer at offset 
-//  return;
-//}
-//
-//function _writePosition(newNextPosition, offset) {
-//  //
-//  // Write new
-//}
+function _write(NodeHook, buffer, offset) {
+  //
+  // Write buffer  on file at offset 
+  //
+  fs.writeSync(NodeHook._fileDescriptor, buffer, 0, buffer.length, offset);
+  return buffer;
+}
 
 module.exports.NodeHook = NodeHook;
 // ---------ONLY---FOR---TEST--------------------
 module.exports._slice = _slice;
+module.exports._write = _write;
--- js/test/testNodeHook.js
+++ js/test/testNodeHook.js
@@ -2,12 +2,27 @@ const test = require('tape');
 const NodeHook = require('../NodeHook.js');
 const fs = require('fs');
 
-test('NodeHook._slice returned value', assert => {
+test('NodeHook slice returned value', assert => {
   const file = fs.openSync('./js/test/testFile', 'r+');
   const actual = NodeHook._slice({_fileDescriptor: file}, 3, 7);
   const expected = Buffer.from('D14B1A7B11', 'hex');
+  fs.closeSync(file);
 
   assert.deepEqual(actual, expected,
     'for start=3 end=4 should return a Buffer that start at file\'s byte 3 at end at file\'s byte 7');
+  assert.end();
+});
+
+test('NodeHook write action executed', assert => {
+  const file = fs.openSync('./js/test/testFile2', 'r+');
+  const len = 100; // is len in byte of testFile2
+  NodeHook._write({_fileDescriptor: file}, Buffer.from('canocani', 'ascii'), 7);
+  const actual = NodeHook._slice({_fileDescriptor: file}, 7, 7 + 'canocani'.length - 1);
+  const expected = Buffer.from('canocani', 'ascii');
+  fs.writeSync(file, new Buffer.alloc(len), 0, len, 0);
+  fs.closeSync(file);
+
+  assert.deepEqual(actual, expected,
+    'write should write buffer at position');
   assert.end();
 });

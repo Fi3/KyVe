d9021d8294298974222c916e3a9e26ff4fdf1b63
fi3 <fmerli1@gmail.com>
2017-03-07 20:54:26 +0100

    .

--- js/Errors.js
+++ js/Errors.js
@@ -10,6 +10,9 @@ class MemoryDbKeyNotInBoucket extends Error {
 class MemoryDbKeyNotFirstInBoucket extends Error {
 }
 
+class MemoryDbIndexOutOfRange extends Error {
+}
+
 class ParseHeaderInvalidInput extends Error {
 }
 
@@ -23,6 +26,7 @@ module.exports.MemoryDbKeyNotInDb = MemoryDbKeyNotInDb;
 module.exports.MemoryDbValueTooLong = MemoryDbValueTooLong;
 module.exports.MemoryDbKeyNotInBoucket = MemoryDbKeyNotInBoucket;
 module.exports.MemoryDbKeyNotFirstInBoucket = MemoryDbKeyNotFirstInBoucket;
+module.exports.MemoryDbIndexOutOfRange = MemoryDbIndexOutOfRange;
 module.exports.ParseHeaderInvalidInput = ParseHeaderInvalidInput;
 module.exports.StoredDbNotSupportedEnv = StoredDbNotSupportedEnv;
 module.exports.StoredDbUpdateNodeValueTooLong = StoredDbUpdateNodeValueTooLong;
